580570f6db24bbb2259a2de8246d848b62d506ac
Hoon <gihoon0907@gmail.com>
2022-12-20 12:39:26 +0900

    random

---
+++ Project/project.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main() {
+	srand(time(NULL));
+	int a;
+	int b;
+	for (int i = 1; i <= 10; i++)
+	{
+		a = rand() % 10;
+		b = rand() % 10;
+	}
+}
